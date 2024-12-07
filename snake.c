0a9dc69072e78d72e347f2a2ea0f2a01a4dcd653
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-12-07 21:00:17 +0200

    adding functions and basic planning

--- snake.c
+++ snake.c
@@ -1,8 +1,50 @@
 #include <conio.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <windows.h>
+#include <time.h>
+
+#define Width 40
+#define height 40
+
+int gameover;
+int x, y, fruitX, fruitY, score;
+int tailx[100], taily[100];
+int tail;
+
+enum direction {stop = 0, left, right, up, down};
+enum direction dir;
+
+void setup(void)
+{
+    gameover = 0;
+    dir = stop;
+    x = Width / 2;
+    y = height / 2;
+    srand(time(0));
+    fruitX = rand() &Width;
+    fruitY = rand() &height;
+}
+
+void draw(void)
+{
+
+}
+
+void function(void)
+{
+
+}
+
+void logic(void)
+{
+    
+}
 
 int main(void)
 {
-    
+    setup();
+
 }
